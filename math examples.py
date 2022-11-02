a38b70dbf0b9a180b9dd1c01acd4b678c9a8d274
Alena <yurevichei@mail.ru>
2022-11-02 20:38:07 +0300

    среднее арифметическое

--- math examples.py
+++ math examples.py
@@ -1,5 +1,11 @@
-from math import pi
+from math import pi, sqrt
 
 R = float(input())
 print(pi * R ** 2)  # площадь круга
 print(2 * pi * R)  # длина окружности
+
+a, b = float(input()), float(input())
+print((a + b) / 2)  # Среднее арифметическое
+print(sqrt(a * b))  # Среднее геометрическое
+print(2 * a * b / (a + b))  # Среднее гармоническое
+print(sqrt((a ** 2 + b ** 2) / 2))  # Среднее квадратичное
