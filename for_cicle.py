14c4b7c974c72190783114dee54ba449c384388d
Alena <yurevichei@mail.ru>
2022-11-28 20:11:39 +0300

    growth of microorganisms

--- for_cicle.py
+++ for_cicle.py
@@ -1,4 +1,10 @@
-n = int(input())
+n = int(input('input number _ '))
 for i in range(n + 1):
     print('*' * n)
     n -= 1
+
+
+m, p, n = int(input('microorganisms_')), int(input('grown in percent_')), int(input('number of days_'))
+for i in range(n):
+    print(i + 1, m)
+    m += m * p / 100
