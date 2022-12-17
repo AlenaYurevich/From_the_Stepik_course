a04857a2ef1c5049e96a4b0a194b689b0e914dc4
Alena <yurevichei@mail.ru>
2022-12-17 20:48:43 +0300

    two tasks

--- for_cicle.py
+++ for_cicle.py
@@ -8,3 +8,27 @@ m, p, n = int(input('microorganisms_')), int(input('grown in percent_')), int(in
 for i in range(n):
     print(i + 1, m)
     m += m * p / 100
+
+"""
+На вход программе подаются два целых числа a и b (a≤b). Напишите программу, которая подсчитывает количество чисел в диапазоне от a до b включительно, куб которых оканчивается на 4 или 9.
+"""
+a, b = int(input()), int(input())
+counter = 0
+for i in range(a, b + 1):
+    m = [4, 9]
+    cube = i**3
+    if cube % 10 in m:
+        counter += 1
+print(counter)
+
+
+"""
+На вход программе подается натуральное число n, а затем n целых чисел, каждое на отдельной строке. Напишите программу, которая подсчитывает сумму введенных чисел. 
+"""
+n = int(input())
+total = 0
+for _ in range(n):
+    a = int(input())
+    total += a
+print(total)
+
