03ce0356d03170389f7fe502a5284a70cc406327
Alena <yurevichei@mail.ru>
2022-12-25 19:47:22 +0300

    processing with digits of number

--- while_cicle.py
+++ while_cicle.py
@@ -59,11 +59,50 @@
 """
 Программа должна вывести число, записанное в обратном порядке
 """
-num = int(input())
-while num != 0:
-    print(num % 10, end='')
-    num = num // 10
+# num = int(input())
+# while num != 0:
+#     print(num % 10, end='')
+#     num = num // 10
 
+"""
+Программа должна вывести максимальную и минимальную цифры введенного числа
+"""
+# max_num = 0
+# min_num = 10
+# num = int(input())
+# while num:
+#     if num % 10 > max_num:
+#         max_num = num % 10
+#     if num % 10 < min_num:
+#         min_num = num % 10
+#     num = num // 10
+# print("Максимальная цифра равна", max_num)
+# print("Минимальная цифра равна", min_num)
 
+"""
+Дано натуральное число. Программа, которая вычисляет:
+сумму его цифр;
+количество цифр в нем;
+произведение его цифр;
+среднее арифметическое его цифр;
+его первую цифру;
+сумму его первой и последней цифры.
+"""
+# num = int(input())
+# num_multiply = 1
+# digits = []
+# while num:
+#     digit = num % 10
+#     digits.append(digit)
+#     num_multiply *= digit
+#     num = num // 10
+# num_total = sum(digits)
+# num_len = len(digits)
+# num_average = num_total / num_len
+# print(num_total, num_len, num_multiply, num_average, digits[-1], digits[-1] + digits[0], sep='\n')
 
-
+"""
+Напишите программу, которая определяет вторую (с начала) цифру.
+"""
+num = str(input())
+print(num[1])
