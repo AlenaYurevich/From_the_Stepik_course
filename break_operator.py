bf11c99770cde36cdd47586360a76fd163ebc8d8
Alena Yurevich <yurevichei@mail.ru>
2023-01-04 20:51:21 +0300

    Task for code review

--- break_operator.py
+++ break_operator.py
@@ -28,18 +28,48 @@
 
 
 """
+Упражнение на ревью кода
+"""
+# max_x = -10**6
+# s = 0
+# for _ in range(10):
+#     x = int(input())
+#     if x < 0:
+#         s += x
+#         if x > max_x:
+#             max_x = x
+# if s < 0:
+#     print(s)
+#     print(max_x)
+# else:
+#     print("NO")
+
 
 """
-max_x = -10**6
-s = 0
-for _ in range(10):
-    x = int(input())
-    if x < 0:
-        s += x
-        if x > max_x:
-            max_x = x
-if s < 0:
-    print(s)
-    print(max_x)
-else:
-    print("NO")
+Упражнение на ревью кода
+"""
+# n = int(input())
+# max_digit = -1
+# if n == 0:
+#     print(n)
+# else:
+#     while n > 0:
+#         digit = n % 10
+#         if digit % 3 == 0:
+#             if digit > max_digit:
+#                 max_digit = digit
+#         n = n // 10
+#     if max_digit == -1:
+#         print('NO')
+#     else:
+#         print(max_digit)
+
+"""
+Упражнение на ревью кода
+"""
+n = int(input())
+max_digit = 0
+while n > 0:
+    max_digit = n % 10
+    n = n // 10
+print(max_digit)
