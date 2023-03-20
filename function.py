580b7bc09059c0f4b034f55536d822c02096c1f4
Alena Yurevich <yurevichei@mail.ru>
2023-03-20 20:47:37 +0300

    draw figures

---
+++ function.py
@@ -0,0 +1,56 @@
+# def draw_box():
+#     print('*' * 10)
+#     for _ in range(12):
+#         print('*' + ' ' * 8 + '*')
+#     print('*' * 10)
+#
+#
+# draw_box()
+#
+
+# def draw_triangle():
+#     for i in range(1, 11):
+#         print('*' * i)
+#
+#
+# draw_triangle()
+
+# def print_text(text, num):
+#     while num > 0:
+#         print(text, end='')
+#         num -= 1
+#
+#
+# print_text('Python', 4)
+
+
+# def draw_triangle(fill, base):
+#     for i in range(1, base // 2 + 2):
+#         print(fill * i)
+#     for i in range(base // 2, 0, -1):
+#         print(fill * i)
+#
+#
+# draw_triangle(input(), int(input()))
+
+
+# def print_fio(name, surname, patronymic):
+#     a = surname[0] + name[0] + patronymic[0]
+#     print(a.upper())
+#
+#
+# name, surname, patronymic = input(), input(), input()
+#
+# # вызываем функцию
+# print_fio(name, surname, patronymic)
+
+# def print_fio(name, surname, patronymic):
+#     print((surname[0] + name[0] + patronymic[0]).upper())
+#
+#
+# name, surname, patronymic = input(), input(), input()
+#
+# # вызываем функцию
+# print_fio(name, surname, patronymic)
+
+
