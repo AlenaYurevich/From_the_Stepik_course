d95753140e8fbf36e04bc0d75be8f889dad303bb
Alena Yurevich <yurevichei@mail.ru>
2023-03-27 20:39:57 +0300

    function with return

--- function.py
+++ function.py
@@ -138,3 +138,73 @@ x = 5
 #
 #
 # print(find_all(s, char))
+
+
+"""
+Напишите функцию merge(list1, list2), которая принимает в качестве аргументов
+два отсортированных по возрастанию списка, состоящих из целых чисел,
+и объединяет их в один отсортированный список
+"""
+
+# def merge(list1, list2):
+#     return sorted(list1 + list2)
+#
+#
+# numbers1 = [int(c) for c in input().split()]
+# numbers2 = [int(c) for c in input().split()]
+#
+#
+# print(merge(numbers1, numbers2))
+
+
+"""
+Быстрое слияние двух отсортированных списков
+"""
+
+
+# def quick_merge(list1, list2):
+#     result = []
+#
+#     p1 = 0  # указатель на первый элемент списка list1
+#     p2 = 0  # указатель на первый элемент списка list2
+#
+#     while p1 < len(list1) and p2 < len(list2):  # пока не закончился хотя бы один список
+#         if list1[p1] <= list2[p2]:
+#             result.append(list1[p1])
+#             p1 += 1
+#         else:
+#             result.append(list2[p2])
+#             p2 += 1
+#
+#     if p1 < len(list1):  # прицепление остатка
+#         result += list1[p1:]
+#     if p2 < len(list2):
+#         result += list2[p2:]
+#
+#     return result
+#
+#
+# list1 = [3, 10, 11, 12, 47, 57, 58, 63, 77, 79, 80, 95]
+# list2 = [0, 11, 12, 20, 24, 26, 47, 48, 53, 65, 70, 81, 84, 84, 90]
+# list3 = quick_merge(list1, list2)
+# print(list3)
+
+
+"""
+На вход программе подается число n, а затем n строк, содержащих целые числа в 
+порядке возрастания. Из данных строк формируются списки чисел. Напишите программу,
+ которая объединяет указанные списки в один отсортированный список с помощью функции quick_merge(), 
+а затем выводит его.
+"""
+
+
+def quick_merge(n=int(input())):
+    res = [input().split() for _ in range(n)]
+    merge_list = []
+    for i in res:
+        for j in i:
+            merge_list.append(int(j))
+    return sorted(merge_list)
+
+
+print(*quick_merge())
