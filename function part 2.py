4e0775c31e9e3925946652221b7d5656a98ef0ec
Alena Yurevich <yurevichei@mail.ru>
2023-04-03 22:01:59 +0300

    function part 3

--- function part 2.py
+++ function part 2.py
@@ -22,11 +22,60 @@ side1, side2, side3 и False в противном случае.
 """
 
 
-def is_prime(num):
-    return 1 < len([1 for i in range(1, num + 1) if num % i == 0]) < 3
+# def is_prime(num):
+#     return len([1 for i in range(1, num + 1) if num % i == 0]) == 2
 
 
-n = int(input())
+# n = int(input())
+#
+#
+# print(is_prime(n))
+
+"""
+Напишите функцию get_next_prime(num), которая принимает в качестве аргумента 
+натуральное число num и возвращает первое простое число большее числа num
+"""
 
 
-print(is_prime(n))
+# def get_next_prime(num):
+#     num += 1
+#     while not is_prime(num):
+#         num += 1
+#     return num
+#
+#
+# n = int(input())
+#
+# print(get_next_prime(n))
+
+"""
+Напишите функцию is_password_good(password), которая принимает в качестве аргумента
+строковое значение пароля password и возвращает значение True если пароль является 
+надежным и False в противном случае.
+"""
+
+
+def is_password_good(password):
+    counter = 0
+    if len(password) > 7:
+        counter += 1
+    for i in password:
+        if i.islower():
+            counter += 1
+            break
+    for i in password:
+        if i.isupper():
+            counter += 1
+            break
+    for i in password:
+        if i.isdigit():
+            counter += 1
+            break
+    return counter == 4
+
+
+txt = input()
+
+print(is_password_good(txt))
+
+#  посмотри форум решений
