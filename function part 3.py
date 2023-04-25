d9349afbd5e5ee736f675f607b8482de9d164068
Alena Yurevich <yurevichei@mail.ru>
2023-04-25 19:45:59 +0300

    tryangle, shipping cost, factorial, numbers to words, seek month

--- function part 3.py
+++ function part 3.py
@@ -23,15 +23,90 @@
 # print(get_circle(r))
 
 
-from math import sqrt
-def solve(a, b, c):
-    d = b ** 2 - 4 * a * c
-    x1, x2 = (-b + sqrt(d)) / (2 * a), (-b - sqrt(d)) / (2 * a)
-    return min(x1, x2), max(x1, x2)
+# from math import sqrt
+# def solve(a, b, c):
+#     d = b ** 2 - 4 * a * c
+#     x1, x2 = (-b + sqrt(d)) / (2 * a), (-b - sqrt(d)) / (2 * a)
+#     return min(x1, x2), max(x1, x2)
+#
+#
+# a, b, c = int(input()), int(input()), int(input())
+#
+#
+# x1, x2 = solve(a, b, c)
+# print(x1, x2)
 
 
-a, b, c = int(input()), int(input()), int(input())
+# объявление функции
+# def draw_triangle():
+#     n = 16
+#     for i in range(1, n, 2):
+#         print(' ' * int((n - i) // 2) + '*' * i)
+#
+#
+# # основная программа
+# draw_triangle()  # вызов функции
+
+# объявление функции
+# def get_shipping_cost(quantity):
+#     return 1000 + 120 * (quantity - 1)
+#
+# # считываем данные
+# n = int(input())
+#
+# # вызываем функцию
+# print(get_shipping_cost(n))
 
 
-x1, x2 = solve(a, b, c)
-print(x1, x2)
+# from math import factorial as f
+#
+#
+# def compute_binom(n, k):
+#     return f(n) // (f(k) * f(n - k))
+#
+#
+# # считываем данные
+# n = int(input())
+# k = int(input())
+#
+# # вызываем функцию
+# print(compute_binom(n, k))
+
+
+# объявление функции
+# def number_to_words(num):
+#     tens = ['десять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать',
+#             'семнадцать', 'восемнадцать', 'девятнадцать']
+#     units = ['ноль', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять']
+#     dec = ['', '', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят',
+#     'девяносто']
+#     if num < 10:
+#         return units[num]
+#     elif num < 20:
+#         return tens[num % 10]
+#     elif num % 10 == 0:
+#         return dec[num // 10]
+#     else:
+#         return dec[num // 10] + ' ' + units[num % 10]
+#
+#
+# # считываем данные
+# n = int(input())
+#
+# # вызываем функцию
+# print(number_to_words(n))
+
+
+def get_month(language, number):
+    en = ['', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
+          'november', 'december']
+    ru = ['', 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь',
+          'ноябрь', 'декабрь']
+    return en[number] if language == "en" else ru[number]
+
+
+lan = input()
+num = int(input())
+
+
+print(get_month(lan, num))
