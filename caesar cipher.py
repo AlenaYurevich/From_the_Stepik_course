0764e97dc34e1bb1186b83355dba881abc192275
Alena Yurevich <yurevichei@mail.ru>
2023-05-25 18:40:18 +0300

    caesar cipher

--- caesar cipher.py
+++ caesar cipher.py
@@ -29,8 +29,95 @@ for i in str1:
         #     str2 += chr(1072 + n - 1103 + ord(i))
     else:
         str2 += i
-print(ord("А"), ord("Я"))
-print(ord("а"), ord("я"))
-print(ord("A"), ord("Z"))
-print(ord("a"), ord("z"))
-print(str2)
+# print(ord("А"), ord("Я"))
+# print(ord("а"), ord("я"))
+# print(ord("A"), ord("Z"))
+# print(ord("a"), ord("z"))
+# print(str2)
+
+
+def caesar_code(text1, step, encrypted_text=''):
+    a = 'abcdefghijklmnopqrstuvwxyz'
+    for i in text1:
+        if i.lower() in a:
+            encrypted_text += a[a.find(i.lower()) - 26 + step]
+        else:
+            encrypted_text += i
+    return encrypted_text.capitalize()
+
+
+text1 = 'To be, or not to be, that is the question!'
+step = 17
+
+print(caesar_code(text1, step))
+
+
+def caesar_code_rus(text2, step, encrypted_text=''):
+    a = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+    for i in text2:
+        if i.lower() in a:
+            encrypted_text += a[a.find(i.lower()) - step]  # обратный сдвиг
+        else:
+            encrypted_text += i
+    return encrypted_text.capitalize()
+
+
+text2 = 'Шсъцхр щмчжмщ йшм, нмтзж йшм лхшщзщг.'
+step = 7
+
+print(caesar_code_rus(text2, step))
+
+
+def caesar_code(text, step, encrypted_text=''):
+    for i in text:
+        if i.isalpha():
+            if (ord(i.lower()) - ord('z') + step) > 0:
+                encrypted_text += chr(ord(i.lower()) - (26 - step))
+            else:
+                encrypted_text += chr(ord(i.lower()) + step)
+        else:
+            encrypted_text += i
+    return encrypted_text.capitalize()
+
+
+text = 'Sgd fqzrr hr zkvzxr fqddmdq nm sgd nsgdq rhcd ne sgd edmbd.'
+step = 25
+print(caesar_code(text, step))
+
+
+def caesar_code(text2, step, encrypted_text=''):
+    a = 'abcdefghijklmnopqrstuvwxyz'
+    for i in text2:
+        if i.lower() in a:
+            encrypted_text += a[a.find(i.lower()) - step]  # обратный сдвиг
+        else:
+            encrypted_text += i
+    return encrypted_text.capitalize()
+
+
+# text2 = 'Hawnj pk swhg xabkna ukq nqj.'
+# step = 1
+# for i in range(1, 26):
+#     print(step)
+#     print(caesar_code(text2, step))
+#     step += 1
+
+
+def caesar_code_3(text, step, encrypted_text=''):
+    a = 'abcdefghijklmnopqrstuvwxyz'
+    res = []
+    words = text.split(" ")
+    print(words)
+    for word in words:
+        for i in word:
+            if i.lower() in a:
+                encrypted_text += a[a.find(i.lower()) + step]  # сдвиг вправо
+            else:
+                encrypted_text += i
+        res.append(encrypted_text)
+    print(res)
+
+
+text = 'Day, mice. "Year" is a mistake!'
+step = 1
+print(caesar_code_3(text, step))
